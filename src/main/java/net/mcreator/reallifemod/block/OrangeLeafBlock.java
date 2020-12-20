20b7ba1c2a4c09fdc3e5c5959995856f1f6c5f25
OreoDeath2 <airid@192.168.1.210>
2020-12-20 15:03:59 +0200

    Naujas Biome

---
+++ src/main/java/net/mcreator/reallifemod/block/OrangeLeafBlock.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.reallifemod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.reallifemod.RealLifeModModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@RealLifeModModElements.ModElement.Tag
+public class OrangeLeafBlock extends RealLifeModModElements.ModElement {
+	@ObjectHolder("real_life_mod:orange_leaf")
+	public static final Block block = null;
+	public OrangeLeafBlock(RealLifeModModElements instance) {
+		super(instance, 14);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.LEAVES).sound(SoundType.PLANT).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("orange_leaf");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
