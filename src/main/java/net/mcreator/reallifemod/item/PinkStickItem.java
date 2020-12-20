ee381468d4ed7b9c7fbb8f8603b8430b934920bb
Oreodeath2 <User1@User1-PC>
2020-12-20 15:04:29 +0200

    stick

---
+++ src/main/java/net/mcreator/reallifemod/item/PinkStickItem.java
@@ -0,0 +1,59 @@
+
+package net.mcreator.reallifemod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.reallifemod.RealLifeModModElements;
+
+import java.util.List;
+
+@RealLifeModModElements.ModElement.Tag
+public class PinkStickItem extends RealLifeModModElements.ModElement {
+	@ObjectHolder("real_life_mod:pink_stick")
+	public static final Item block = null;
+	public PinkStickItem(RealLifeModModElements instance) {
+		super(instance, 14);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("pink_stick");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("stick"));
+		}
+	}
+}
