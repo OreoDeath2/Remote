9563121ff89c4be2cfdaed08387cab4af3196fce
OreoDeath2 <airid@192.168.1.210>
2020-12-20 15:13:16 +0200

    Viskas ka mes padarem

---
+++ src/main/java/net/mcreator/reallifemod/item/StickpinkItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.reallifemod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.reallifemod.RealLifeModModElements;
+
+@RealLifeModModElements.ModElement.Tag
+public class StickpinkItem extends RealLifeModModElements.ModElement {
+	@ObjectHolder("real_life_mod:stickpink")
+	public static final Item block = null;
+	public StickpinkItem(RealLifeModModElements instance) {
+		super(instance, 16);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MATERIALS).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("stickpink");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
